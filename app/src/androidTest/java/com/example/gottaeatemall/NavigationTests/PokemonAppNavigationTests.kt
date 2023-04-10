b68e07b0cfb4e507484c1ef263a2976ba1aa707f
NickCasey27 <72876282+NickCasey27@users.noreply.github.com>
2023-04-09 20:45:35 -0500

    Starting navigation testing

---
+++ app/src/androidTest/java/com/example/gottaeatemall/NavigationTests/PokemonAppNavigationTests.kt
@@ -0,0 +1,53 @@
+package com.example.gottaeatemall.NavigationTests
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.performClick
+import androidx.navigation.NavController
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import com.example.gottaeatemall.App
+import com.example.gottaeatemall.AppScreen
+import com.example.gottaeatemall.onNodeWithStringId
+import com.example.gottaeatemall.R
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class PokemonAppNavigationTests {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupCupcakeNavHost(){
+        composeTestRule.setContent {
+            navController =
+                TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(
+                ComposeNavigator()
+            )
+            App(navController = navController)
+        }
+    }
+
+    fun NavController.assertCurrentRouteName(expectedRouteName: String){
+        Assert.assertEquals(expectedRouteName, currentBackStackEntry?.destination?.route)
+    }
+
+    @Test
+    fun pokemonNavHost_verifyStart(){
+        navController.assertCurrentRouteName(AppScreen.Home.name)
+    }
+
+    @Test
+    fun pokemonNavHost_navigateToSearchScreen() {
+        composeTestRule.onNodeWithStringId(R.string.page_search)
+            .performClick()
+        navController.assertCurrentRouteName(AppScreen.Search.name)
+    }
+}
