5172c8ccc042cdb98fec1c843dc71562696e77b2
EthanHeuer <heuere9166@my.uwstout.edu>
2023-04-14 18:11:34 -0500

    Set colors

--- app/src/main/java/com/example/gottaeatemall/ui/theme/Color.kt
+++ app/src/main/java/com/example/gottaeatemall/ui/theme/Color.kt
@@ -2,6 +2,13 @@ package com.example.gottaeatemall.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// https://coolors.co/e3442b-484a47-5c6d70-e0e0e2-cfd6ea
+val Red = Color(0xFFE3442B)
+val DarkGray = Color(0xFF484A47)
+val Gray = Color(0xFF5C6D70)
+val LightGray = Color(0xFFE0E0E2)
+val LightBlue = Color(0xFFCFD6EA)
+
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
