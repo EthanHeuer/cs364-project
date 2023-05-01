d2a3d43b59ea42ad1513b22e1044bf2000d7907b
AnTrinh2864 <118484971+AnTrinh2864@users.noreply.github.com>
2023-04-30 23:34:34 -0500

    Update PokemonUIState.kt

--- app/src/main/java/com/example/gottaeatemall/data/PokemonUIState.kt
+++ app/src/main/java/com/example/gottaeatemall/data/PokemonUIState.kt
@@ -1,9 +1,16 @@
 package com.example.gottaeatemall.data
 
+import android.graphics.BitmapFactory
+import androidx.compose.ui.graphics.ImageBitmap
+
 data class PokemonUIState(
     val ingredients: List<String> = listOf(),
     val trainerName: String = "",
-    val trainerBadgeCouzznt: Int = 0,
+    //val trainerBadgeCount: Int = 0,
     val favoritePokemonUse: String = "",
-    val favoritePokemonEat: String = ""
-)
+    val favoritePokemonEat: String = "",
+    //val playtime:String = "",
+    val totalCaught:String = "",
+    var badges: List<String> = listOf(),
+    var boy_or_girl: Boolean = true
+    )
