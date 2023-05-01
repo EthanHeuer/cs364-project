03b8964667d6359ee391b3af251b490fcb3cdaa5
AnTrinh2864 <118484971+AnTrinh2864@users.noreply.github.com>
2023-04-30 23:33:09 -0500

    Update PokemonViewModel.kt

--- app/src/main/java/com/example/gottaeatemall/ui/screens/PokemonViewModel.kt
+++ app/src/main/java/com/example/gottaeatemall/ui/screens/PokemonViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.gottaeatemall.ui.screens
 
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.lifecycle.ViewModel
 import com.example.gottaeatemall.data.PokemonUIState
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -13,6 +14,7 @@ class PokemonViewModel: ViewModel() {
     val uiState: StateFlow<PokemonUIState> = _uiState.asStateFlow()
 
     var _ingredientList = mutableListOf<String>()
+    var _badge = mutableListOf<String>()
     fun addIngredient(ingredient: String){
         if (_ingredientList.contains(ingredient)){
             _ingredientList.remove(ingredient)
@@ -28,8 +30,38 @@ class PokemonViewModel: ViewModel() {
         }
     }
 
+    fun setBadge(badge:String) {
+        if (_badge.contains(badge)){
+            _badge.remove(badge)
+            _uiState.update { currentState -> currentState.copy(badges = _badge) }
+        } else {
+            _badge.add(badge)
+            _uiState.update { currentState -> currentState.copy(badges = _badge)}
+        }
+    }
     fun resetOrder(){
         //_uiState.value = PokemonUIState()
         _ingredientList = mutableListOf()
+    }
+    fun resetBadge() {
+        _badge = mutableListOf()
+    }
+    fun setName(trainer:String) {
+        _uiState.update { currentState -> currentState.copy(trainerName = trainer) }
+    }
+//    fun setPlayTime(playtime:String) {
+//        _uiState.update { currentState -> currentState.copy(playtime = playtime) }
+//    }
+    fun setNumCaught(numCaught:String) {
+        _uiState.update { currentState -> currentState.copy(totalCaught = numCaught) }
+    }
+    fun setFavBattle(favBattle:String) {
+        _uiState.update { currentState -> currentState.copy(favoritePokemonUse = favBattle) }
+    }
+    fun setFavEat(favEat:String) {
+        _uiState.update { currentState -> currentState.copy(favoritePokemonEat = favEat) }
+    }
+    fun setGender(gender:Boolean) {
+        _uiState.update { currentState -> currentState.copy(boy_or_girl = gender) }
     }
 }
