b5d81e7ac393e8ecfa4a2e20b16abd39af119109
AnTrinh2864 <118484971+AnTrinh2864@users.noreply.github.com>
2023-05-04 11:26:40 -0500

    Delete DetailScreen.kt

--- app/src/main/java/com/example/gottaeatemall/ui/screens/DetailScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.gottaeatemall.ui.screens
-
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun DetailScreen() {
-    Text("Detail Screen")
-}
